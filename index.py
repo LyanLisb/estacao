139041b68e637f51446775fc811461646e0a0774
LyanLisb <lyan240406@gmail.com>
2024-09-11 16:55:22 -0300

    8

--- index.py
+++ index.py
@@ -1,11 +1,9 @@
 from flask import Flask, render_template
 
-#from config import config_all
+from config import config_all
 
 app = Flask(__name__)
 
-#config_all(app)
-@app.get('/')
-def _ ():
-    return render_template("climaatual.html")
+config_all(app)
+
 
